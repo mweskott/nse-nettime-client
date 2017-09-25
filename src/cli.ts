9bc662731eca5980a7ba6e69e048c5018fe5c8e5
Bastian Koch <bastian.koch@cegeka.de>
2017-09-25 17:49:01 +0200

    More tolerance for wrong input

--- src/cli.ts
+++ src/cli.ts
@@ -2,8 +2,6 @@ import * as program from "commander";
 import { Nettime } from "./nettime";
 import { ZeitkontierungPage } from "./zeitkontierung-page";
 
-
-
 program
   .option('--url <url>', 'Nettime server URL')
   .option('-u, --user <login>', 'User login')
@@ -14,26 +12,29 @@ program
   .description('submit booking')
   .action((number: string, date: string, timeStart: string, timeEnd: string) => {
 
-    let url = program.url;
-    let user = program.user;
-    let password = program.password;
+    let cfg = require('./config.json');
+    let url = program.url || cfg.url;
+    let user = program.user || cfg.username;
+    let password = program.password || cfg.password;
 
     console.log("book", url, user, password);
     console.log("book", number, date, timeStart, timeEnd);
 
 
-    let nettime = new Nettime(program.url);
-    nettime.contact().then(result => {
-      console.log("contact then", result);
-      result.login(program.user, program.password).then(result => {
-        console.log("login then", result);
+    let nettime = new Nettime(url);
+    if(!!number && !!date && !!timeStart && !!timeEnd) {
+      nettime.contact().then(result => {
+        console.log("contact then", result);
+        result.login(user, password).then(result => {
+          console.log("login then", result);
 
-        let bookingPage = new ZeitkontierungPage(result);
-        bookingPage.buchen(number, date, timeStart, timeEnd).then((page) => {
-          console.log("... gebucht");
+          let bookingPage = new ZeitkontierungPage(result);
+          bookingPage.buchen(number, date, timeStart, timeEnd).then((page) => {
+            console.log("... gebucht");
+          });
         });
       });
-    });
+    }
 
 
   });
