92391839468187e3255ba83536198f3cb4983097
Markus Weskott <markus.weskott@cegeka.de>
2017-09-26 08:33:15 +0200

    custom configuration file by cmd parameter

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,37 @@
 import * as program from "commander";
 import { Nettime } from "./nettime";
 import { ZeitkontierungPage } from "./zeitkontierung-page";
+import fs = require('fs');
+
+class Configuration {
+  url: string;
+  username: string;
+  password: string;
+}
 
 program
   .option('--url <url>', 'Nettime server URL')
   .option('-u, --user <login>', 'User login')
-  .option('-p, --password <password>', 'Password');
+  .option('-p, --password <password>', 'Password')
+  .option('-c, --config <configFile>', 'Configuration file');
 
 program
   .command('book [number] [date] [timeStart] [timeEnd]')
   .description('submit booking')
   .action((number: string, date: string, timeStart: string, timeEnd: string) => {
 
-    let cfg = require('./config.json');
+    let cfg = new Configuration();
+    if (program.config) {
+      var contents = fs.readFileSync(program.config);
+      cfg = JSON.parse(contents.toString());
+    }
+
     let url = program.url || cfg.url;
     let user = program.user || cfg.username;
     let password = program.password || cfg.password;
 
     console.log("book", url, user, password);
     console.log("book", number, date, timeStart, timeEnd);
-
 
     let nettime = new Nettime(url);
     if(!!number && !!date && !!timeStart && !!timeEnd) {
